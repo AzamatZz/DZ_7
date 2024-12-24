eb158824861008381f206bfc91096aff7c9db488
AzamatZz <vip.aza19921992@gmail.com>
2024-12-24 22:17:29 +0600

    DZ7

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from './features/tasksSlice';
-import counterReducer from './features/counterSlice';
 
 const store = configureStore({
   reducer: {
     tasks: tasksReducer,
-    counter: counterReducer,
   },
 });
 
